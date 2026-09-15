c3a5cace8eb2f926605bbc6a7cfff1adbd95840f
kanchankambalkar <kanchankambalkar2007@gmail.com>
2026-09-15 15:06:48 +0530

    A simple Python project that generates random and strong passwords based on the length entered by the user.

---
+++ random_password_generator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+# Ask the user for password length
+length = int(input("Enter password length: "))
+
+# Characters to use
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate password
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+# Display password
+print("Your generated password is:", password)
